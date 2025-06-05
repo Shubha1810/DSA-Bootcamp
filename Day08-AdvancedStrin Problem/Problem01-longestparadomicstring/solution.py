b9f2acaab31fcbb00b686825da609335e2fddc62
Shubha Desai <127526108+Shubha1810@users.noreply.github.com>
2025-06-05 07:40:03 +0000

    changes in

--- Day08-AdvancedStrin Problem/Problem01-longestparadomicstring/solution.py
+++ Day08-AdvancedStrin Problem/Problem01-longestparadomicstring/solution.py
@@ -10,18 +10,26 @@ def longest_palindromic_String(str1):
     max_len = 0
     result = ""
 
-    for i in range(len(str1) - 1):  
-        left = i
-        right = i + 1
-        while left >= 0 and right < len(str1):
-            if(str1[left] == str1[right]):
-                if (right - left + 1) > max_len:
-                    max_len = right - left + 1
-                    result = str1[left:right+1]
-                left -= 1
+    for i in range(len(str1)):
+        # Odd-length palindrome (centered at i)
+        left, right = i, i
+        while left >= 0 and right < len(str1) and str1[left] == str1[right]:
+            if (right - left + 1) > max_len:
+                max_len = right - left + 1
+                result = str1[left:right+1]
+            left -= 1
+            right += 1
+
+        # Even-length palindrome (centered between i and i+1)
+        left, right = i, i + 1
+        while left >= 0 and right < len(str1) and str1[left] == str1[right]:
+            if (right - left + 1) > max_len:
+                max_len = right - left + 1
+                result = str1[left:right+1]
+            left -= 1
             right += 1
 
     return result
 
 s1 = get_strings()
-print("Longest Even-Length Palindromic Substring:", longest_palindromic_String(s1))
+print("Longest Palindromic Substring:", longest_palindromic_String(s1))
